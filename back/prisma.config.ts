0b345bf52e00a410f5ef3346a401da0930a66275
Łukasz <109240153+Barabasz1@users.noreply.github.com>
2026-06-12 12:27:07 +0200

    prisma migrations

--- back/prisma.config.ts
+++ back/prisma.config.ts
@@ -1,12 +1,14 @@
 // prisma.config.ts
-export default {
-  schema: "prisma/schema.prisma",
+import { defineConfig } from 'prisma/config';
+
+export default defineConfig({
+  schema: 'prisma/schema.prisma',
   migrations: {
-    path: "prisma/migrations",
+    path: 'prisma/migrations',
   },
   datasource: {
     url:
-      process.env["DATABASE_URL"] ??
-      "postgresql://admin:password123@localhost:5432/nestjs_db?schema=public",
+      process.env.DATABASE_URL ??
+      'postgresql://admin:password123@localhost:5432/nestjs_db?schema=public',
   },
-};
+});
